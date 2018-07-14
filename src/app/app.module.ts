c7bb0c86e37e9bfe21defc68b0366dc356aa3408
TheTedD <thetedd.one@gmail.com>
2018-07-14 16:35:24 +0600

    delete two components
    1. my.component
    2. my2.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {MyComponent} from './my/my.component';
-import { My2Component } from './my2/my2.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MyComponent,
-        My2Component
     ],
     imports: [
         BrowserModule
