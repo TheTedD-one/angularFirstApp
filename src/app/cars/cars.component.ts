e0847a0619e2feeec380f96ec19c71942da69e0e
TheTedD <thetedd.one@gmail.com>
2018-07-15 15:49:43 +0600

    one way binding

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-cars',
-  templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+    selector: 'app-cars',
+    templateUrl: './cars.component.html',
+    styleUrls: ['./cars.component.css']
 })
-export class CarsComponent implements OnInit {
+export class CarsComponent {
 
-  constructor() { }
+    addCaqrStatus = '';
+    inputText = '';
 
-  ngOnInit() {
-  }
+    constructor() {
 
+    }
+
+    addCar() {
+        this.addCaqrStatus = 'yez';
+    }
+
+    onKeyUp(event) {
+        this.inputText = event.target.value;
+    }
+
+    onKeyUp2(val) {
+        this.inputText = val;
+    }
 }
