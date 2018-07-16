df70eefb60e4b70a8425d24036c31dbf44d0fe88
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-16 17:39:15 +0600

    Передача параметра в компонент

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -5,26 +5,35 @@ import {Component} from '@angular/core';
     templateUrl: './cars.component.html',
     styleUrls: ['./cars.component.css']
 })
+
 export class CarsComponent {
 
-    carName = '';
-    addCarStatus = false;
-    cars = ['Ford', 'Audi', 'Bmw', 'Mazda', 'Lada', 'Bently'];
-    items = [
-        new Date(2015, 3, 8),
-        new Date(2016, 7, 1),
-        new Date(2017, 10, 6),
-        new Date(2018, 37, 5),
-    ]
+    carName: string;
+    carYear: number;
+    cars: [{name: string, year: number}] = [
+        {
+            name: 'Ford',
+            year: 2005,
+        },
+        {
+            name: 'Mazda',
+            year: 2008,
+        },
+        {
+            name: 'Mercedes-benz',
+            year: 2015,
+        },
+    ];
 
     constructor() {
 
     }
 
     addCar() {
-        this.addCarStatus = true;
-        this.cars.push(this.carName);
-        this.carName = '';
+        this.cars.push({
+            name: this.carName,
+            year: this.carYear,
+        });
     }
 
 
