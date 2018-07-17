6655fb3b996f51937c33816f49feb0bcc5876283
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-17 16:40:42 +0600

    clear project & pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,39 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    template: `
+        <div class="col-xs-8 col-xs-offset-2">
+            <h2>{{ name }}</h2>
+            <h2>{{ name | lowercase }}</h2>
+            <h2>{{ name | uppercase }}</h2>
+            <h2>{{ name | slice:0:3 | uppercase }}</h2>
+            <hr>
+            <h2>{{ pi }}</h2>
+            <h2>{{ pi | number:'1.2-2' }}</h2>
+            <hr>
+            <h2>{{ money | currency:'USD':false }}</h2>
+            <hr>
+            <h2>{{ date | date:'yyyy.MM.dd' }}</h2>
+            <hr>
+            <h2>{{ amount | percent }}</h2>
+            <hr>
+            <pre>{{ object | json }}</pre>
+        </div>
+    `
 })
 export class AppComponent {
-    headerText = 'Создание своей директивы';
-    items = [1, 2, 3, 4, 5];
-    current = 1;
-
-    onClick(item: number) {
-        this.current = item;
-    }
+    name = 'Ruslan';
+    pi = Math.PI;
+    money = 350;
+    date = new Date();
+    amount = 0.45;
+    object = {
+        foo: 'bar',
+        baz: 'qux',
+        nested:
+            {
+                xyz: 3,
+                numbers: [1, 2, 3]
+            }
+    };
 }
