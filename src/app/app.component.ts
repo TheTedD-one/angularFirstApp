c5a5912157ce2a204ad73bfc2ce503b5271a6275
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-17 17:21:55 +0600

    pure pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {Component} from '@angular/core';
     template: `
         <div class="col-xs-8 col-xs-offset-2">
             <input class="form-control" type="text" [(ngModel)]="searchCar">
+            <button class="btn btn-primary" (click)="addCar()">Добавить</button>
             <hr>
             <ul class="list-group">
                 <li *ngFor="let item of items | carFilter:searchCar; let idx = index" class="list-group-item"><b>{{ idx + 1 }}</b> - {{ item.name }}</li>
@@ -22,4 +23,8 @@ export class AppComponent {
         {name: 'BMW'},
         {name: 'Mercedes'},
     ];
+
+    addCar() {
+        this.items.push({name: 'new car'});
+    }
 }
