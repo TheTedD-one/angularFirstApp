11a2963913db530814d6bab2d4d9abc1126589a9
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-18 17:30:39 +0600

    http get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import {Component, OnInit} from '@angular/core';
 import {CarsService} from './cars.service';
+import {Response} from '@angular/http';
+
+interface Cars {
+    name: string;
+    color: string;
+    id: number;
+}
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    cars = [];
+    cars: Cars[] = [];
 
     constructor(private carsService: CarsService) {}
 
     loadCars() {
-        this.carsService.getCars().subscribe((response) => {
-            console.log(response);
+        this.carsService.getCars().subscribe((cars: Cars[]) => {
+            this.cars = cars;
         });
     }
 }
--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+import {map} from 'rxjs/internal/operators';
 
 @Injectable()
 
@@ -7,6 +8,6 @@ export class CarsService {
     constructor(private http: Http) {}
 
     getCars() {
-        return this.http.get('http://localhost:3000/cars');
+        return this.http.get('http://localhost:3000/cars').pipe(map((response: Response) => response.json()));
     }
 }
