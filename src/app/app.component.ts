24baa99eeff10262dd9f54bce63039484a8ef055
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-18 16:40:37 +0600

    http get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,18 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {reject} from 'q';
+import {CarsService} from './cars.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-    cars = [
-        {
-            name: 'Ford',
-            color: 'White',
-            id: 1
-        }
-    ];
+    cars = [];
+
+    constructor(private carsService: CarsService) {}
 
     ngOnInit() {
-
+        this.carsService.getCars().subscribe((response) => {
+            console.log(response);
+        });
     }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {CarsService} from './cars.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     ReactiveFormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
