0154e45b8ce9b063f3a75c23c4fb45cca3f67dbe
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-18 11:00:19 +0600

    Использование сервиса в сервисе

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,4 +1,10 @@
+import {ConsoleService} from './console.service';
+import {Injectable} from '@angular/core';
+
+@Injectable()
+
 export class CarsService {
+    constructor(private consoleService: ConsoleService) {}
     cars = [
         {
             name: 'Ford',
@@ -19,5 +25,6 @@ export class CarsService {
             name,
             isSold: false
         });
+        this.consoleService.log('Машина ' + name + ' добавлена!');
     }
 }
