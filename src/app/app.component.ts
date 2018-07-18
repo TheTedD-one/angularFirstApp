f614828977ce10b51a7881df0fc58ef5f1123426
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-18 11:02:36 +0600

    clearProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {CarsService} from './cars.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-
-    cars = [];
-
-    constructor(private service: CarsService) {}
-
     ngOnInit() {
-        this.cars = this.service.cars;
+
     }
 }
