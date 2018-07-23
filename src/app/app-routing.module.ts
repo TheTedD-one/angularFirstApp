9add416bf84697799518332e362ad5eb9e7b766c
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-23 14:12:17 +0600

    параметры адресной строки часть 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
 import {CarsPageComponent} from './cars-page/cars-page.component';
+import {CarPageComponent} from './car-page/car-page.component';
 
 const appRoutes: Routes = [
+    { path: '', component: HomePageComponent },
     { path: 'cars', component: CarsPageComponent },
-    { path: '', component: HomePageComponent }
+    { path: 'cars/:id/:name', component: CarPageComponent },
 ];
 
 @NgModule({
