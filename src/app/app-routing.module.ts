68caf9acf2c51aa8ffada4dc65795a3255752382
TheTedD <thetedd.one@gmail.com>
2018-07-23 20:17:47 +0600

    Guard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import {HomePageComponent} from './home-page/home-page.component';
 import {CarsPageComponent} from './cars-page/cars-page.component';
 import {CarPageComponent} from './car-page/car-page.component';
 import {NotFoundComponent} from './not-found/not-found.component';
+import {AuthGuard} from './auth-guard.service';
+import {NewPageComponent} from './new-page/new-page.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomePageComponent },
-    { path: 'cars', component: CarsPageComponent, children: [
+    { path: 'new', component: NewPageComponent },
+    { path: 'cars', component: CarsPageComponent, canActivate: [AuthGuard], children: [
         { path: ':id/:name', component: CarPageComponent },
     ]},
     {path: 'not-found', component: NotFoundComponent},
