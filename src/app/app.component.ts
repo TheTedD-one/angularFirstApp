a4bebd03f7a3012ed659b85a14aaf301daab8b3a
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-26 15:27:51 +0600

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('clickDiv', [
+      state('start', style({
+        backgroundColor: 'blue',
+        width: '150px',
+        height: '150px'
+      })),
+      state('end', style({
+        backgroundColor: 'red',
+        width: '300px',
+        height: '300px'
+      })),
+      transition('start => end', animate(1500)),
+      transition('end => start', animate('0.8s 0.5s ease-out'))
+    ]),
+  ]
 })
 export class AppComponent {
+  clickedDivState = 'start';
 
+  changeDivState() {
+    this.clickedDivState = 'end';
+    setTimeout(() => {
+      this.clickedDivState = 'start';
+    }, 3000);
+  }
 }
