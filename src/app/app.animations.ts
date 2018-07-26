d02727cb5ccce6977f4c0a13485cd45df26cf1a3
TheTedD <thetedd.one@gmail.com>
2018-07-26 20:15:24 +0600

    group && keyframes

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -1,28 +1,33 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {animate, group, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 export const divTrigger = trigger('divTrigger', [
   // void => * (:enter)
   transition(':enter', [
     style({
-      opacity: 0
+        width: '*',
+        height: '*'
     }),
-    animate(500, style({
-      opacity: 1
-    }))
+    group([
+      animate(3000, style({
+        width: '200px',
+        height: '200px',
+      })),
+      animate(6000, keyframes([
+        style({
+          backgroundColor: 'blue'
+        }),
+        style({
+          backgroundColor: 'yellow'
+        }),
+        style({
+          backgroundColor: 'green'
+        })
+      ])),
+    ]),
+    animate(1000)
   ]),
   // * => void (:leave)
   transition(':leave', animate(500, style({
       opacity: 0
   })))
-]);
-
-export const changeWidthTrigger = trigger('divWidthTrigger', [
-  transition('* => *', [
-    animate(1000, style({
-      width: '10px'
-    })),
-    animate(1000, style({
-      width: '*'
-    })),
-  ])
 ]);
