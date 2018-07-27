85a6d09c2177d05be59ba570bfa95fc5644cb599
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-27 16:28:59 +0600

    clearProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { divTrigger} from './app.animations';
-import { AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [divTrigger]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isVisible = false;
-
-  onAnimStart($event: AnimationEvent) {
-    console.log('start');
-  }
-
-  onAnimDone($event: AnimationEvent) {
-    console.log('done');
-  }
+  title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    BrowserAnimationsModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
